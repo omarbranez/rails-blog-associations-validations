a1419f2adb4bff5bb5caf060ec059c58d99f5307
Omar <omar.branez@gmail.com>
2021-06-19 15:28:04 -0400

    commit tree 6a9afb8d5ffeb1c6f7e90450b097a075a7d0c07e

--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.integer :user_id
       t.string :name
-
+      t.text :content
       t.timestamps null: false
     end
   end
